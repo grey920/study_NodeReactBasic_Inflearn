d1c2a6a9361b4daf6656cf1f0847b239843e6def
grey920 <gyuwoonj@gmail.com>
2021-01-24 22:11:07 +0900

    32 로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import axios from 'axios';
 
 
-function LandingPage() {
+function LandingPage(props) {
 
     // LandingPage에 들어오자마자 여기 useEffect를 실행한다.
     // 
@@ -11,6 +11,16 @@ function LandingPage() {
         .then(response => console.log(response.data))
     }, []);
 
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+        .then(response => {
+            if(response.data.success){
+                props.history.push('/login')
+            } else {
+                alert('로그아웃에 실패했습니다')
+            }
+        })
+    }
 
     return (
         <div style={{
@@ -21,6 +31,9 @@ function LandingPage() {
             height:'100vh'
         }}>
             <h2>시작 페이지</h2>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
